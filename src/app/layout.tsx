94edf079044c038890ac894fd24331d2566a44ab
Uday Sagar <udaysagar.mail@gmail.com>
2024-10-13 03:12:44 +0530

    refactor: add logo to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Logo from "@/assets/uday.png";
 
 export const metadata: Metadata = {
   title: "Image Gallery",
   description: "Search and view images from Unsplash",
+  icons: {
+    icon: Logo.src,
+  },
 };
 
 export default function RootLayout({
