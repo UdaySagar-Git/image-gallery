876ba4decc011712c7c566ffeddbd6592f3151cd
Uday Sagar <udaysagar.mail@gmail.com>
2024-10-13 01:59:03 +0530

    refactor: Update ImageModal component to improve UI and functionality

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import React from "react";
 import { UnsplashImage } from "@/app/page";
 
 interface ImageModalProps {
@@ -10,16 +9,21 @@ interface ImageModalProps {
 export function ImageModal({ image, onClose }: ImageModalProps) {
   return (
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/75"
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/75 p-4 md:p-8"
       onClick={onClose}
     >
-      <Image
-        src={image.urls.regular}
-        alt={image.alt_description}
-        className="max-h-[80vh] max-w-4xl rounded-lg object-contain"
-        width={image.width}
-        height={image.height}
-      />
+      <div
+        className="relative max-h-full max-w-full overflow-hidden rounded-lg bg-white"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <Image
+          src={image.urls.regular}
+          alt={image.alt_description}
+          className="max-h-[80vh] w-auto object-contain"
+          width={image.width}
+          height={image.height}
+        />
+      </div>
     </div>
   );
 }
